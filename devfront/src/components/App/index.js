f52ff1698a98ac5e293a30937b0dc9137ec12191
Noloxe-fred <noloxe.web@gmail.com>
2019-05-31 22:28:24 +0200

    Ajout App avec Navbar

--- devfront/src/components/App/index.js
+++ devfront/src/components/App/index.js
@@ -5,11 +5,35 @@ import 'semantic-ui-css/semantic.min.css';
 
 // == Import : local
 import './app.scss';
+import NavBar from 'src/containers/NavBar';
+// import Home from 'src/containers/Home';
+// import FullList from 'src/containers/FullList';
+// import Search from 'src/containers/Search';
+// import AdvancedSearch from 'src/containers/AdvancedSearch';
 
 // == Composant
 const App = () => (
   <div id="app">
-    App React
+    <NavBar />
+    {/* <Switch>
+      <Route
+        exact
+        path="/"
+        component={Home}
+      />
+      <Route
+        path="/fulllist/:slug"
+        component={FullList}
+      />
+      <Route
+        path='search'
+        component={Search}
+      />
+      <Route
+        path='advancedsearch'
+        component={AdvancedSearch}
+      />
+    </Switch> */}
   </div>
 );
 
