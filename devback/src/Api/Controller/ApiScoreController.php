8c771446b2ba31b0f5e99fd8fbb4d9412e263e5e
benjaminsrd <sicardbenj@gmail.com>
2019-06-04 10:30:49 +0200

    suppression et modification d'un score ok + recalcul de la moyenne

--- devback/src/Api/Controller/ApiScoreController.php
+++ devback/src/Api/Controller/ApiScoreController.php
@@ -53,10 +53,11 @@ class ApiScoreController extends FOSRestController
         $numberVotes = count($game->getScores());
         $currentScore = $game->getScore();
         $newValue = $currentScore + $request->request->get('value');
-        $newScore = self::averageScore($newValue, $numberVotes);
+        $newAverage = self::averageScore($newValue, $numberVotes);
+
         // Ajout des valeurs à game
-        $game->setAverageScore($newScore);
         $game->setScore($newValue);
+        $game->setAverageScore($newAverage);
 
         $em->persist($game);
         $em->persist($score);
@@ -65,7 +66,37 @@ class ApiScoreController extends FOSRestController
         return new JsonResponse('', JsonResponse::HTTP_CREATED);
     }
 
-       /**
+    /**
+     * @Rest\View
+     * @Rest\Put(path = "/score/edit", name="score_edit")
+     */
+    public function editScoreAction(Request $request, EntityManagerInterface $em, ScoreRepository $scoreRepository, GameRepository $gameRepository, UserRepository $userRepository)
+    {
+
+        $user = $userRepository->findOneById($request->request->get('user', 'id'));
+        $game = $gameRepository->findOneById($request->request->get('game', 'id'));
+        $score = $scoreRepository->findOneById($request->request->get('id'));
+
+        $scoreNewValue = $request->request->get('value');
+
+        // Calcul de la moyenne de la note du jeu
+        $numberVotes = count($game->getScores()) - 1;
+        $currentScore = $game->getScore();
+        $newValue = ($currentScore - $score->getValue()) + $scoreNewValue;
+
+        $newAverage = self::averageScore($newValue, $numberVotes);
+
+        // Modifie les valeurs à game et score
+        $score->setValue($scoreNewValue);
+        $game->setScore($newValue);
+        $game->setAverageScore($newAverage);
+
+        $em->flush();
+
+        return new JsonResponse('', JsonResponse::HTTP_OK);
+    }
+
+    /**
      * @Rest\View
      * @Rest\Delete(path = "/score/delete", name="score_delete")
      */
@@ -80,10 +111,12 @@ class ApiScoreController extends FOSRestController
         $numberVotes = count($game->getScores()) - 2;
         $currentScore = $game->getScore();
         $newValue = $currentScore - $score->getValue();
-        $newScore = self::averageScore($newValue, $numberVotes);
+        
+        $newAverage = self::averageScore($newValue, $numberVotes);
+
         // Supprime des valeurs à game
-        $game->setAverageScore($newScore);
         $game->setScore($newValue);
+        $game->setAverageScore($newAverage);
 
         $em->remove($score);
         $em->flush();
