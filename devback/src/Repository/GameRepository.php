4f60db020bd40547c5ae84fcbb33e6de166a9bd4
AhmedoclockQ <daoudiahmed.mid@gmail.com>
2019-06-04 15:30:02 +0200

    merge 3

--- devback/src/Repository/GameRepository.php
+++ devback/src/Repository/GameRepository.php
@@ -103,18 +103,18 @@ class GameRepository extends ServiceEntityRepository
         return $qb;
     }
 
-    // /!\ Méthode non terminé /!\ //
-    public function filterGamesByCategory(array $array){
+    // // /!\ Méthode non terminé /!\ //
+    // public function filterGamesByCategory(array $array){
 
-        $qb = $this->createQueryBuilder('g')
-        ->where(':array MEMBER OF g.categories')
-        //->where('expr->eq('g.categories', '?1') MEMBER OF g.categories')
-        ->setParameter('array', array_values($array))
-        ->getQuery()
-        ->getResult();
+    //     $qb = $this->createQueryBuilder('g')
+    //     ->where(':array MEMBER OF g.categories')
+    //     //->where('expr->eq('g.categories', '?1') MEMBER OF g.categories')
+    //     ->setParameter('array', array_values($array))
+    //     ->getQuery()
+    //     ->getResult();
 
-        return $qb;
-    }
+    //     return $qb;
+    // }
 
     // public function findGamesByBestScore(){
 
