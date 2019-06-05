4d7978e908d100b84e18ef474ca309b4d82657af
AhmedoclockQ <daoudiahmed.mid@gmail.com>
2019-06-05 16:24:36 +0200

    auth

--- devback/src/Api/Controller/ApiUserController.php
+++ devback/src/Api/Controller/ApiUserController.php
@@ -13,13 +13,21 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Validator\ConstraintViolationList;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
 
 class ApiUserController extends FOSRestController
 {
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+        {
+            $this->passwordEncoder = $passwordEncoder;
+        }
 
     /**
      * @Rest\View
-     * @Rest\Post(path = "/user/new", name="user_new")
+     * @Rest\Post(path = "/signup", name="user_new")
      * @ParamConverter(
      *      "user",
      *      converter="fos_rest.request_body",
@@ -28,15 +36,19 @@ class ApiUserController extends FOSRestController
      *      }
      * )
      */
-    public function newUserAction(Request $request, EntityManagerInterface $em, User $user, ConstraintViolationList $violations)
+    public function newUserAction(Request $request, EntityManagerInterface $em, User $user, ConstraintViolationList $violations, UserPasswordEncoderInterface $passwordEncoder)
     {   
         if(count($violations)){
             
             return $this->view($violations, Response::HTTP_BAD_REQUEST);
         }
+
+
         $user = new User();
+        $clearpassword =$request->request->get('password');
+        $hash = $this->passwordEncoder->encodePassword($user, $clearpassword);
         $user->setEmail($request->request->get('email'));
-        $user->setPassword($request->request->get('password'));
+        $user->setPassword($hash);
         $user->setPseudo($request->request->get('pseudo'));
         
         $em->persist($user);
