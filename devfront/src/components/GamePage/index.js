6f6772fef4d0c6672a690bf72185f7a8affcfeb0
Maximilien Renaut <max.renaut@gmail.com>
2019-06-08 14:03:32 +0200

    proposition nouveau layout

--- devfront/src/components/GamePage/index.js
+++ devfront/src/components/GamePage/index.js
@@ -1,13 +1,9 @@
 import React, { Component } from 'react';
 import Proptypes from 'prop-types';
+import { Container, Row, Col } from 'react-bootstrap';
 
+import oneGame from '../../data/oneGame';
 import './gamepage.scss';
-
-import GameHeader from 'src/containers/GamePage/gameHeaderContainer';
-import GameComments from 'src/containers/GamePage/gameCommentsContainer';
-import GameAllComments from 'src/containers/GamePage/gameAllCommentsContainer';
-
-
 
 // dans cette classe des props vont etre accessible par le container qui sont loading et request game 
 class GamePage extends Component {
@@ -32,19 +28,53 @@ class GamePage extends Component {
         {loading && <div>Chargement</div>}
         {!loading && (
           <div id="gamepage" style={backgroundStyle}>
-            <GameHeader />
-            <GameComments />
-            <GameAllComments />
-          </div>
-        )}
+            <Container fluid>
+              <Row className="back--button">
+                <Col lg={12} md={12} sm={12} xs={12} className="d-flex justify-content-flex-start">
+                  <button type="button">back</button>
+                </Col>
+              </Row>
+              <Row className="two--sides">
+                <Col lg={6} md={12} sm={12} xs={12} className="left--part">
+                 <div className="game">
+                    <div className="up">
+                      <div className="up--left">
+                        <img src={oneGame.cover} alt="cover" />
+                      </div>
+                      <div className="up--right">
+                        <h3>{oneGame.name}</h3>
+                        <h3>{oneGame.score}%</h3>
+                        <h5>Éditeur : {oneGame.editor}</h5>
+                        <h5> Développeur : {oneGame.developer}</h5>
+                        <h5>Date de sortie : {oneGame.released}</h5>
+                        <a href="#">Ajouter à la collection</a>
+                      </div>
+                    </div>
+                    <div className="down">
+                      <h6>{oneGame.description}</h6>  
+                    </div>
+                 </div>             
+                </Col>
+                <Col lg={6} md={12} sm={12} xs={12} className="right--part">
+                  <div className="comments--list">
+                    
+                  </div>
+                </Col>
+              </Row>
+            </Container>
+          </div> 
+          )
+        }
       </div>
-    );
+    )
   }
-}
+};
+       
 
 GamePage.propTypes = {
   requestGame: Proptypes.func.isRequired,
   loading: Proptypes.bool.isRequired,
+  background: Proptypes.string.isRequired,
 };
 
 export default GamePage;
