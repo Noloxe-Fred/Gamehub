a13a78b9e96dd39e169ff3b41aa1885ce6180295
Noloxe-fred <noloxe.web@gmail.com>
2019-06-10 21:35:19 +0200

    creation bis

--- devfront/src/Library/FullComponent/AddGame.js
+++ devfront/src/Library/FullComponent/AddGame.js
@@ -1,4 +1,36 @@
 import React, { Component } from 'react';
+import Proptypes from 'prop-types';
 
 import './addgame.scss';
+import { verifyHave } from '../../store/reducers/addGameReducer';
 
+class AddGame extends Component {
+
+  componentDidMount() {
+    verifyHave(this.props.gameId);
+  }
+
+  render() {
+    const {
+      loadVerify,
+      alreadyHave,
+      available,
+      loadSubmit,
+      openPopover,
+      receivedSubmit,
+      messageNotCheck 
+    } = this.props;
+
+    return (
+      <div className="add-modal">
+        
+      </div>
+    );
+  }
+}
+
+AddGame.propTypes = {
+
+};
+
+export default AddGame;
