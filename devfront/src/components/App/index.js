f84ea12d442b4dac4064786de5e342d94257b60a
Noloxe-fred <noloxe.web@gmail.com>
2019-06-16 21:35:53 +0200

    Test Profile partout off/ Requete axios score OK

--- devfront/src/components/App/index.js
+++ devfront/src/components/App/index.js
@@ -46,56 +46,8 @@ class App extends Component {
       <div id="app">
         {redirectSearch && <Redirect to="/search" />}
         {/* {sessionStorage.getItem('disconnect') && <Redirect to="/" />} */}
-        <Navbar />
+        <Navbar /> 
         <Switch>
-        <Sidebar.Pushable>
-
-        <Sidebar as={Segment} animation="overlay" direction="top" visible={displayedProfile}>
-          <Header as='h3'>PROFIL</Header>
-          <Grid textAlign='center' className="profile-content">
-                <div>
-                  <h4>Paramètres</h4>
-                  <h6>Vos informations</h6>
-                  <Segment>
-                    <p>Votre pseudo:</p><Button>Modifier votre pseudo</Button>
-                    <p>Votre email:</p>
-                  </Segment>
-                  <Segment>
-                    <h6>Changer votre mot de passe</h6>
-                    <Form>
-                      <Input
-                        placeholder="Ancien mot de passe"
-                      />
-                      <Input
-                        placeholder="Nouveau mot de passe"
-                      />
-                      <Input
-                        placeholder="Confirmation nouveau mot de passe"
-                      />
-                      <Button type="submit">Modifier</Button>
-                    </Form>
-                  </Segment>
-                </div>
-                <div>
-                  <h4>Stats</h4>
-                  <h6>Votre collection</h6>
-                  <Segment>
-                    <p>Vous avez 37 jeux en votre possession</p>
-                    <p>Vous avez 12 jeux en liste d'achats</p>
-                    <p>Vous suivez 12 jeux</p>
-                  </Segment>
-                </div>
-                <div>
-                  <h4>Commentaires</h4>
-                  <Segment>Commentaire 1</Segment>
-                  <Segment>Commentaire 2</Segment>
-                  <Segment>Commentaire 3</Segment>
-                  <Segment>Commentaire 4</Segment>
-                </div>
-          </Grid>
-        </Sidebar>
-        <Sidebar.Pusher dimmed={displayedProfile}>
-  
           <Route
             exact
             path="/"
@@ -120,10 +72,7 @@ class App extends Component {
           <Route
             component={Page404}
           />
- 
-        </Sidebar.Pusher>
-      </Sidebar.Pushable>
-      </Switch>
+        </Switch>
         <Footer />
       </div>
     );
